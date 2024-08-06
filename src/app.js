b6893d0886dbf76214a289aee7454cf2589854fe
SamFiore <francomarcelocordoba@gmail.com>
2024-08-06 20:30:46 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const boton = document.querySelector("#boton")
+const menu = document.querySelector("#menu")
+
+boton.addEventListener("click",()=>{menu.classList.toggle('hidden')})
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './src/pruebas1.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
